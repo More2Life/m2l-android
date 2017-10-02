5228d690972265fb8a9220937a8098e2ed846806
Todd Wilson <todd@dodgertodd.com>
2017-10-02 12:19:26 -0700

    Refactor into components

--- App.js
+++ App.js
@@ -2,71 +2,9 @@ import React, {Component} from 'react';
 import {
     StyleSheet,
     Text,
-    View,
-    Image,
-    ScrollView,
-    Alert,
-    TouchableNativeFeedback,
-    FlatList
+    View
 } from 'react-native';
-
-class FeedItem extends Component {
-    _onPress = () => {
-        this.props.onPressItem(this.props.feedItem._id);
-        // Alert.alert('You tapped the button!')
-    };
-
-    render() {
-        let image = {
-            uri: this.props.feedItem.feedImageUrl
-        };
-        return (
-            <View style={{flex: 1}}>
-                {/* <Text>{'Selected: ' + this.props.selected}</Text> */}
-                <Text style={styles.title}>{this.props.feedItem.title}</Text>
-                <TouchableNativeFeedback onPress={this._onPress}>
-                    <Image source={image} style={{
-                        flex: 1,
-                        aspectRatio: 1
-                    }}/>
-                </TouchableNativeFeedback>
-            </View>
-        );
-    }
-}
-
-class FeedItemList extends React.PureComponent {
-    state = {selected: (new Map(): Map<string, boolean>)};
-
-    _onPressItem = (_id: string) => {
-        Alert.alert('You tapped the button!');
-
-        this.setState((state) => {
-            const selected = new Map(state.selected);
-            selected.set(_id, !selected.get(_id)); // toggle
-            return {selected};
-        });
-    };
-
-    _renderItem = ({item}) => (
-        <FeedItem
-            feedItem = {item}
-            onPressItem = {this._onPressItem}
-            selected = {!!this.state.selected.get(item._id)}
-        />
-    );
-
-    render() {
-        return (
-            <FlatList
-                data={this.props.data}
-                keyExtractor={item => item._id}
-                renderItem={this._renderItem}
-                extraData={this.state}
-            />
-        )
-    }
-}
+import FeedItemList from './components/FeedItemList';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -141,5 +79,3 @@ const styles = StyleSheet.create({
         marginHorizontal: 5
     }
 });
-
-// it chaged
